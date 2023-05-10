1b637f088b076ea9cccf343c114fd120c1f876ef
Felipe Batista E Baptista <38279457801@info.local>
2023-05-10 17:21:28 -0300

    fazer a atividade do jogador

---
+++ 1_Semestre/Backend/Atividades/ListCarro/Program.cs
@@ -0,0 +1,29 @@
+﻿// Criar um projeto de console.
+// Criar uma classe Carro, com marca e cor.
+// Criar um construtor vazio e um completo.
+// Receber dados no console para adicionar 2 objetos em uma lista.
+// Exibir os dois objetos da lista no console.
+
+using ListCarro;
+
+List<Carro> carros = new List<Carro>();
+
+carros.Add(
+    new Carro("Ferrari", "Vermelha")
+);
+
+carros.Add(
+    new Carro("Mustang", "Azul")
+);
+
+Carro primeiro = new Carro("Ferrari","Vermelha");
+
+Carro segundo = new Carro("Mustang", "Azul");
+
+Console.WriteLine($"Lista de carros da minha garagem");
+Console.WriteLine($"********************************");
+
+foreach (var item in carros)
+{
+    Console.WriteLine($"Marca: {item.Marca}, cor: {item.Cor} ");
+}
---
+++ 1_Semestre/Backend/Exemplos em aula/ListasObjetos/Program.cs
@@ -0,0 +1,48 @@
+﻿// Crie uma classe Produto com as propriedades: int Código, string Nome, float Preco.
+// Crie um construtor vazio.
+// Crie um construtor completo.
+
+using ListasObjetos;
+
+List<Produto> produtos = new List<Produto>();
+
+produtos.Add(
+    new Produto(2563, "QuickSilver", 19.90f)
+);
+
+produtos.Add(
+    new Produto(1025, "Tenis NAiki", 50f)
+);
+
+Produto calcaDiesel = new Produto(1526, "calça Disel Calvo klein", 15f);
+
+produtos.Add(calcaDiesel);
+
+Console.WriteLine($"Lista Antiga");
+Console.WriteLine($"****************");
+
+foreach (var item in produtos)
+{
+    Console.WriteLine($"Código: {item.Codigo}, Nome: {item.Nome}, Preço: {item.Preco:C} {produtos.IndexOf(item)}");
+}
+
+// Update
+
+Produto produtoBuscado = produtos.Find(x => x.Codigo == 2563);
+
+int index = produtos.IndexOf(produtoBuscado);
+
+produtoBuscado.Preco = 199.90f;
+
+produtos.RemoveAt(index);
+
+produtos.Insert(index, produtoBuscado);
+
+Console.WriteLine($"Lista Atualizada");
+Console.WriteLine($"****************");
+
+foreach (var item in produtos)
+{
+    Console.WriteLine($"Código: {item.Codigo}, Nome: {item.Nome}, Preço: {item.Preco:C} {produtos.IndexOf(item)}");
+}
+
