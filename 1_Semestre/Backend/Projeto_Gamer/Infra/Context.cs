8e2e6ea8763b4a86dd4d4047655579898d5b280d
Felipe Batista E Baptista <38279457801@info.local>
2023-05-29 17:22:13 -0300

    database

---
+++ 1_Semestre/Backend/Projeto_Gamer/Infra/Context.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Projeto_Gamer.Models;
+
+namespace Projeto_Gamer.Infra
+{
+    public class Context : DbContext
+    {
+        public Context()
+        {
+        }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = NOTE11-S14; initial catalog = gamer; Integrated Security = true; TrustServerCertificate = true");
+            }
+        }
+
+        public DbSet<Jogador> Jogador { get; set; }
+        public DbSet<Equipe> Equipe { get; set; }
+    }
+}
